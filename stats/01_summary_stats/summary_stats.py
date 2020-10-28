740bb297711f591c62b4819b9c5bdc47dbcf06e7
Tovio Roberts <clownfragment@gmail.com>
2020-10-27 20:16:44 -0600

    Auto Updated: Tue 16:20 Oct 27 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -71,7 +71,9 @@ Calculate the Median of both groups
 urban = [6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0]
 farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0]
 
+# print(sorted(urban))
+# print(f'Median Urban: {round(median(urban), 1)}')
+# print(sorted(farmhouse))
+# print(f'Median Farmhouse: {round(median(farmhouse), 1)}')
 
-print(f'Median Urban: {round(median(urban), 1)}')
 
-print(f'Median Farmhouse: {round(median(farmhouse), 1)}')
