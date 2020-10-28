8f2ba4575817e8c7f603dd0b9c09cf5bfa8411e3
Tovio Roberts <clownfragment@gmail.com>
2020-10-27 19:38:54 -0600

    Auto Updated: Tue 38:19 Oct 27 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -7,9 +7,8 @@ def mean(lst, trim_by=0):
     if trim_by > 0:
         lst_ = sorted(lst_)[trim_by:-trim_by]
 
-    print(lst_)
     return sum(lst_) / len(lst_)
 
 a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 
-print(mean(a, trim_by=1))
+# print(mean(a, trim_by=1))
