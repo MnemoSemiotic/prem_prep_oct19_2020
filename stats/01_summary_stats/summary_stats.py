efa6a7b01d62f557c76a217b966939c2eaca38ac
Tovio Roberts <clownfragment@gmail.com>
2020-10-27 19:47:28 -0600

    Auto Updated: Tue 47:19 Oct 27 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -12,3 +12,25 @@ def mean(lst, trim_by=0):
 a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 
 # print(mean(a, trim_by=1))
+
+
+'''
+Breakout Slide 10
+
+An article published in the journal, Indoor Air, considered two different air samples to test for endotoxin concentrations, the first in urban households, and the second in farmhouses.
+'''
+urban =  [6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0]
+farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0, 0.3]
+
+'''
+A. Determine the sample mean for each group.
+'''
+print(f'Sample Mean Urban: {round(mean(urban), 1)}')
+print(f'Sample Mean Farmhouse: {round(mean(farmhouse), 1)}')
+
+'''
+B. Determine the trimmed mean for each group by trimming the smallest and largest value from each group.
+'''
+
+print(f'Sample Trimmed Mean Urban: {round(mean(urban, trim_by=1), 1)}')
+print(f'Sample Trimmed Mean Farmhouse: {round(mean(farmhouse,trim_by=1), 1)}')
