e8eb702fc3c7aeb87cbce940cd6f94a9487683e4
Tovio Roberts <clownfragment@gmail.com>
2020-10-27 21:01:56 -0600

    Auto Updated: Tue 01:21 Oct 27 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -144,8 +144,8 @@ def five_number_summary(lst):
 a = [15,2,9,5,6,7,27,12,18,19,1]
 b = [6,1,4,51,7,16,10,14,46,22,24,56,48,54]
 
-print(sorted(a))
-print(five_number_summary(a))
-print('\n')
-print(sorted(b))
-print(five_number_summary(b))
+# print(sorted(a))
+# print(five_number_summary(a))
+# print('\n')
+# print(sorted(b))
+# print(five_number_summary(b))
