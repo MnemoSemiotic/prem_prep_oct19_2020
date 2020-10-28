3f703ce436bddea8097d9211b88dc9b817b31e8b
Tovio Roberts <clownfragment@gmail.com>
2020-10-27 20:02:33 -0600

    Auto Updated: Tue 02:20 Oct 27 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -25,15 +25,15 @@ farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0
 '''
 A. Determine the sample mean for each group.
 '''
-print(f'Sample Mean Urban: {round(mean(urban), 1)}')
-print(f'Sample Mean Farmhouse: {round(mean(farmhouse), 1)}')
+# print(f'Sample Mean Urban: {round(mean(urban), 1)}')
+# print(f'Sample Mean Farmhouse: {round(mean(farmhouse), 1)}')
 
 '''
 B. Determine the trimmed mean for each group by trimming the smallest and largest value from each group.
 '''
 
-print(f'Sample Trimmed Mean Urban: {round(mean(urban, trim_by=1), 1)}')
-print(f'Sample Trimmed Mean Farmhouse: {round(mean(farmhouse,trim_by=1), 1)}')
+# print(f'Sample Trimmed Mean Urban: {round(mean(urban, trim_by=1), 1)}')
+# print(f'Sample Trimmed Mean Farmhouse: {round(mean(farmhouse,trim_by=1), 1)}')
 
 
 '''
@@ -41,4 +41,24 @@ Median
 '''
 
 def median(lst):
-    
+    lst_sorted = sorted(lst)
+
+    # if odd
+    if len(lst) % 2 == 1:
+        return lst_sorted[int(len(lst)/2)]
+    # if even
+    else:
+        upper_idx = int(len(lst)/2)
+        lower_val = lst_sorted[upper_idx - 1]
+        upper_val = lst_sorted[upper_idx]
+        return mean([lower_val, upper_val])
+
+
+odd = [13, 18, 13, 14, 13, 16, 14, 21, 13]
+even = [15, 14, 10, 8, 12, 8, 16, 13]
+
+print(sorted(odd))
+print(median(odd))
+
+print(sorted(even))
+print(median(even))
