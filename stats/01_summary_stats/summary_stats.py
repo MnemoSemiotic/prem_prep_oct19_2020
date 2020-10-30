bb669c469a107cc264e101a1fb4bcb86a89b0929
Tovio Roberts <clownfragment@gmail.com>
2020-10-29 20:04:41 -0600

    Auto Updated: Thu 04:20 Oct 29 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -268,4 +268,4 @@ def remove_outliers(lst, outlier_coef=1.5):
 
 house_prices = [590, 615, 575, 608, 350, 1285, 408, 540, 555, 679]
 
-print(remove_outliers(house_prices))
+# print(remove_outliers(house_prices))
