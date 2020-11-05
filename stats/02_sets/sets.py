19ba3fa07150e4a597b0bbb08ea083b8ca49b16c
Tovio Roberts <clownfragment@gmail.com>
2020-11-04 20:57:11 -0700

    Auto Updated: Wed 57:20 Nov 04 2020

--- stats/02_sets/sets.py
+++ stats/02_sets/sets.py
@@ -201,4 +201,13 @@ for outcome in samp_space:
     if outcome[0] == 1:
         A.append(outcome)
 
-print(A)
+# print(A)
+# [[1, 'H', 'H'], [1, 'H', 'T'], [1, 'T', 'H'], [1, 'T', 'T']]
+
+# B = The event in which at least one of the coin flips results in heads
+B = []
+for outcome in samp_space:
+    if outcome.count('H') >= 1:
+        B.append(outcome)
+
+print(B)
