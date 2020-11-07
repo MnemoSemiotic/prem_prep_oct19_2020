b05ab79990768acb821a0572d50bb1aac5ac614a
Tovio Roberts <clownfragment@gmail.com>
2020-11-07 12:21:09 -0700

    Auto Updated: Sat 21:12 Nov 07 2020

---
+++ stats/03_basic_probability/axioms_of_probability.py
@@ -0,0 +1,4 @@
+setA = set(['bear', 'cat', 'dog', 'dolphin', 'weasel'])
+setB = set(['bear', 'dog', 'elephant', 'weasel', 'mink', 'mountain lion'])
+setC = set(['bear', 'whale', 'sea cucumber', 'mink', 'eagle', 'dog'])
+
