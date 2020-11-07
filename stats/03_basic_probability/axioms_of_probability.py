493e9c5d3024413f5c9ae857c6e109bf6fb27fad
Tovio Roberts <clownfragment@gmail.com>
2020-11-07 13:04:18 -0700

    Auto Updated: Sat 04:13 Nov 07 2020

--- stats/03_basic_probability/axioms_of_probability.py
+++ stats/03_basic_probability/axioms_of_probability.py
@@ -7,7 +7,7 @@ sample_space = setA.union(setB).union(setC)
 
 '''
 Commutative
-A U B = B U A
+A ∪ B = B ∪ A
 AB = BA
 '''
 # print(setA.union(setB) == setB.union(setA))
@@ -20,12 +20,15 @@ c = True
 # print((a or b) == (b or a))
 # print((a and b) == (b and a))
 
+
+
 '''
 Associative
-(A U B) U C = A U (B U C)
-(AB)C = A(BC)  
+(A ∪ B) ∪ C = A ∪ (B ∪ C) = A ∪ B ∪ C 
+⇒ 5 + (6 + 7) = (5 + 6 ) + 7 = 5 + 6 + 7
+(AB)C = A(BC) = ABC
 '''
-
+# print((setA.union(setB)).union(setC) == (setC.union(setB)).union(setA))
 # print((setA.union(setB)).union(setC) == (setC.union(setB)).union(setA))
 
 a = True
@@ -36,84 +39,105 @@ c = True
 
 
 '''
-Distributive 
-A U (BC) = (A U B)(A U C)
-A(B U C) = (AB) U (AC)
-'''
-
-# print(setA.union(setB.intersection(setC)) == (setA.union(setB)).intersection(setA.union(setC)))
-
-# print(a or (b and c) == (a and b) or (a and c))
-
-
-'''
-Idempotent Laws
+Distributive
+A ∪ (BC) = (A ∪ B)(A ∪ C) 
+A(B ∪ C) = (AB) ∪ (AC)
+5*(2 * 3) = (5 * 2) + (5 * 3)
 '''
 a = True
 b = False
 c = True
 
-print((a and a) == a)
+# print(setA.union(setB.intersection(setC)) == (setA.union(setB)).intersection(setA.union(setC)))
+
+# print((a or (b and c) == (a and b) or (a and c)))
+
+
+
+'''
+Idempotent Laws
+A ∪ A = A
+AA = A
+'''
+a = True
+b = False
+c = True
+
+# print(setA.union(setA) == setA)
+# print((a and a) == a)
+
 
 
 '''
 Domination Laws
+Aside: 
+U = Universal Set
+The set of which all other subsets are a subset of
+∅  = Empty Set = { }
+A ∪ U = U
+A ∩ U = A
+A ∩ ∅ = ∅
 '''
-print(setA.intersection(sample_space) == setA)
+a = True
+b = False
+c = True
+
+# print(setA.intersection(sample_space) == setA)
 
 null_set = set()
-print(setA.intersection(null_set) == null_set)
-
+# print(setA.intersection(null_set) == null_set)
 
 U = {True, False}
 A = {True}
 
-print(A.intersection(U) == A)
+# print(A.intersection(U) == A)
 
 
 
 '''
 Absorption Laws
+A ∪ (AB) = A
+A(A ∪ B) = A
 '''
+# print(setA.union(setA.intersection(setB)) == setA)
+# print(setA.intersection(setA.union(setB)) == setA)
 
 a = True
 b = False
 c = True
 
-print(a or (a and b) == a)
-print(a and ( a or b) == a)
+# print(a or (a and b) == a)
+# print(a and (a or b) ==  a)
 
 
 '''
-Identity Prop
+Identity Property
+A ∪ ∅ = A
 '''
-print(setA.union(null_set) == setA)
-
-
+# print(setA.union(null_set) == setA)
 
 
 '''
-Complement Laws
-null^c = Universal
-Universal^c = null
+Complement Laws for Universal and Empty Set
+∅^c = U
+U^c = ∅
 '''
-
 
 '''
 Involution Law
+(A^c)^c = A
 '''
-print(not not a == a)
 
 
 '''
-AB U AB^c == A
+AB ∪ AB^c = A
 '''
-print((a and b) or ( a and not b) == a)
-
+# print(((a and b) or (a and not b)) == a)
 
 '''
-DeMorgan's Laws
+DeMorgan’s Laws
+1st: (A ∪ B)^c = A^cB^c
+2nd: (AB)^c = A^c ∪ B^c
 '''
-print((not (a or b)) == ((not a) and (not b)))
-
-print((not (a and b)) == ((not a) or (not b)))
+# print((not (a or b)) == (not a) and (not b))
+# print(not (a and b) == (not a) or (not b))
