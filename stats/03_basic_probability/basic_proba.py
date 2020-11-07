d599dc4790d306187c49acec626d83d280be3566
Tovio Roberts <clownfragment@gmail.com>
2020-11-07 13:15:34 -0700

    Auto Updated: Sat 15:13 Nov 07 2020

--- stats/03_basic_probability/basic_proba.py
+++ stats/03_basic_probability/basic_proba.py
@@ -4,3 +4,10 @@ def coin_flip():
     flip = ['H', 'T']
     return choice(flip)
 
+
+'''
+Write a function called series_of_flips, that has one parameter, n, which represents the number of coin flips. Return a list of the random coin flips.
+['H', 'H', 'T', 'H']
+'''
+def series_of_flips(n):
+    pass
