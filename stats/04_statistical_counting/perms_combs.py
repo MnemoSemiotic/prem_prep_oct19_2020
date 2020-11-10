28daa7d5e4f0dfee43002891ff11f2ac48edd982
Tovio Roberts <clownfragment@gmail.com>
2020-11-09 20:14:30 -0700

    Auto Updated: Mon 14:20 Nov 09 2020

--- stats/04_statistical_counting/perms_combs.py
+++ stats/04_statistical_counting/perms_combs.py
@@ -54,4 +54,21 @@ def permutations(n, k):
 
 '''
 Permutations Intuition
+
+Five pets and 5 pet beds. What are all the ways that you can arrange
+those 5 pets in 5 beds?
 '''
+
+base_5 = ['bat', 'cat', 'frog', 'eel', 'hamster']
+
+animals_counting = []
+
+for an1 in base_5:
+    for an2 in base_5:
+        for an3 in base_5:
+            for an4 in base_5:
+                for an5 in base_5:
+                    animals_counting.append([an1, an2,an3,an4,an5])
+
+for an_number in animals_counting:
+    print(an_number)
