c36a7df4ba351bba54503474d5f6650c4f468871
Tovio Roberts <clownfragment@gmail.com>
2020-11-09 20:38:31 -0700

    Auto Updated: Mon 38:20 Nov 09 2020

--- stats/04_statistical_counting/perms_combs.py
+++ stats/04_statistical_counting/perms_combs.py
@@ -117,3 +117,33 @@ Combs Intuition
 Out of a set of 21 basketball players, only 5 can be on the court at any given time. What are all the combinations possible for that basketball team?
 '''
 # print(combinations(21, 5))
+
+'''
+What are all the combinations of teams that can be on the court
+'''
+# an expensive approach
+
+num_combs = combinations(21, 5)
+
+def basketball_combs():
+    twentyone_nums = range(1, 21+1)
+
+    # every arrangement of 5
+    possible_five = []
+
+    for i in twentyone_nums:
+        for j in twentyone_nums:
+            for k in twentyone_nums:
+                for l in twentyone_nums:
+                    for m in twentyone_nums:
+                        possible_five.append([i,j,k,l,m])
+    
+    permutations = []
+
+    for five in possible_five:
+        if len(list(set(five))) == 5:
+            permutations.append(five)
+
+    combinations = []
+
+    for 
