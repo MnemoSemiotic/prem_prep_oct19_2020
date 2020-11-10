858b6c3111ad49820913195db423454d86865525
Tovio Roberts <clownfragment@gmail.com>
2020-11-09 20:30:47 -0700

    Auto Updated: Mon 30:20 Nov 09 2020

--- stats/04_statistical_counting/perms_combs.py
+++ stats/04_statistical_counting/perms_combs.py
@@ -96,3 +96,8 @@ Combination
 
 nCk = n! / ((n-k)! * k!)
 '''
+def combinations(n, k):
+    return int(factorial(n) / ((factorial(n-k) * factorial(k))))
+
+''' How many 5 card combos from a 52 card deck?'''
+print(combinations(52, 5))
