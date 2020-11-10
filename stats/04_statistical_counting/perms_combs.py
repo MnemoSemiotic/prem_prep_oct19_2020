f719706737bac810356d2149d9539b0eb2aeebff
Tovio Roberts <clownfragment@gmail.com>
2020-11-09 19:41:39 -0700

    Auto Updated: Mon 41:19 Nov 09 2020

--- stats/04_statistical_counting/perms_combs.py
+++ stats/04_statistical_counting/perms_combs.py
@@ -10,3 +10,11 @@ def factorial(num):
 # print(factorial(5))
 
 
+'''
+Factorial Breakout Slide 10
+There are ten people standing in a line for beignets at the world famous cafe du monde in New Orleans. How many different ways could the ten people be arranged in the queue?
+
+
+Given that the line was formed organically (i.e, people got into line as they arrived without any organization or coordination), what is the probability that they are standing in the queue in alphabetical order. Assume everyone has a different last name?
+
+'''
