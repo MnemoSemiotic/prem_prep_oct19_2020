efbb4fe7c9069d76125a1c3957ebd4b27c445023
Tovio Roberts <clownfragment@gmail.com>
2020-11-10 21:05:04 -0700

    Auto Updated: Tue 05:21 Nov 10 2020

--- stats/05_discrete_distributions/binomial/binomial_counting.py
+++ stats/05_discrete_distributions/binomial/binomial_counting.py
@@ -15,34 +15,48 @@ def gen_4_bit_binary():
 
 
 '''
-7 minutes
-
-Code the dec_to_bin function.
+7 minutes!
+Code the dec_to_bin function. 
 Should take as input 2 things:
     a decimal value (dec)
     a number of bits (n_bits=8)
-
 algorithm
 Given a decimal number
     - take the modulus by 2
-        set aside the result
+        - set aside the result
     - floor divide the number by 2
-        until that number is 0
-    - reverse the sequence of number that was set aside
+        - until that number is 0
+    - reverse the sequence of outcomes
+dec_to_bin(43)
+--------------
+43 % 2     ==> 1
+43 // 2 = 21
+21 % 2     ==> 1
+21 // 2 = 10
+10 % 2     ==> 0
+10 // 2 = 5
+5 % 2      ==> 1
+5 // 2  = 2
+2 % 2      ==> 0
+2 // 2  = 1
+1 % 2      ==> 1
+1 // 2  = 0
+reversed ==> 101011 is the binary representation of 43
 '''
 
-def dec_to_bin(dec, num_bits=8):
+def dec_to_bin(dec, n_bits=8):
     bin_list = []
     x = dec
 
-    for _ in range(num_bits):
+    for _ in range(n_bits):
         bit = x % 2
         bin_list.append(bit)
         x //= 2
 
     return bin_list[::-1] # list(reversed(bin_list))
 
-# print(dec_to_bin(43))
+# print(dec_to_bin(43))  # [0, 0, 1, 0, 1, 0, 1, 1]
+
 
 
 def get_binary(n_bits=8):
@@ -53,33 +67,30 @@ def get_binary(n_bits=8):
 
     return bins_d
 
-
-# for dec, bin_ in get_binary(n_bits=16).items():
+# for dec, bin_ in get_binary().items():
 #     print(f'{dec}: {bin_}')
 
 
-
 '''
-Construct binomial distr for n trials with prob=0.5 for each trial
+Construct the binomial distr for n trials with prob=0.5 for each trial
 '''
-
 def binomial_distr(n_trials=8):
     binomial_dict = dict()
 
     bin_dict = get_binary(n_bits=n_trials)
 
-    for _, binary in bin_dict.items():
-        sum_bits = sum(binary)
+    for _, val in bin_dict.items():
+        sum_bits = sum(val)
         if sum_bits not in binomial_dict:
             binomial_dict[sum_bits] = 0
         binomial_dict[sum_bits] += 1
-
+    
     return binomial_dict
 
-d = binomial_distr(n_trials=8)
+d = binomial_distr(n_trials=12)
 
 # for k, v in d.items():
 #     print(f'{k}: {v}')
 
 for k, v in d.items():
-    print(f'{k}: {round(v / sum(d.values()), 4)}')
+    print(f'{k}: {v / sum(d.values())}')
