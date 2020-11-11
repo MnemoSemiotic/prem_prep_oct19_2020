7e8be79b17a1886f13fa5931378ecf9296005644
Tovio Roberts <clownfragment@gmail.com>
2020-11-10 19:47:00 -0700

    Auto Updated: Tue 47:19 Nov 10 2020

--- stats/05_discrete_distributions/binomial/bernoulli.py
+++ stats/05_discrete_distributions/binomial/bernoulli.py
@@ -19,3 +19,48 @@ If you have a coin that on average turns up heads in 90% of its flips, what is t
 0.9
 '''
 
+'''
+A Binomial Process is a series of Bernoulli trials
+where we keep a fixed probability p
+
+The Binomials distr considers the sum of successes in a series of
+Bernoulli trials
+
+"Series of coin flips"
+"Failures of multiples of the same component in parallel to each other"
+
+
+
+"How many ways can you arrange 1 1-bit in 5-bit binary?"
+00001
+00010
+00100
+01000
+10000
+
+"How many ways can you arrange 2 1-bits in 5-bit binary?"
+00011
+00101
+00110
+01001
+...
+
+The Binomial Distr Question
+If you have n Bernoulli trials in a row, what is the 
+probability that k of them are successful?
+
+You flip a coin 20 times. What is the proba that you
+get 14 heads out of 20 flips?
+
+You have 20 components in a parallel to each other. The
+proba that any given component is functioning upon observation
+is 0.5. What is the proba that 14 out of the 20 components is
+functioning upon observation?
+
+0000001111111111111111
+0000010111111111111111
+1000001111111111111110
+
+'''
+
+
