a895b366e7ab73305a5c3bc725df2807eacb0fb6
Tovio Roberts <clownfragment@gmail.com>
2020-11-10 20:49:50 -0700

    Auto Updated: Tue 49:20 Nov 10 2020

--- stats/05_discrete_distributions/binomial/binomial_using_pmf.py
+++ stats/05_discrete_distributions/binomial/binomial_using_pmf.py
@@ -1,8 +1,13 @@
 def factorial(n):
-    pass
+    prod = 1
+
+    for num in range(1, n+1):
+        prod *= num
+
+    return prod
 
 def combinations(n, k):
-    pass
+    return int(factorial(n) / (factorial(n-k) * factorial(k)))
 
 def binomial_pmf(n, k, p=0.5):
     pass
