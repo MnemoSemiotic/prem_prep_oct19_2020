74b844cdbe24001202706da515d507d20b4b0cb9
Tovio Roberts <clownfragment@gmail.com>
2020-11-10 20:58:36 -0700

    Auto Updated: Tue 58:20 Nov 10 2020

--- stats/05_discrete_distributions/binomial/binomial_using_pmf.py
+++ stats/05_discrete_distributions/binomial/binomial_using_pmf.py
@@ -14,3 +14,11 @@ def binomial_pmf(n, k, p=0.5):
 
 # print(binomial_pmf(8, 4, p=0.5)) # 0.2734375
 
+
+'''
+What is the probability in 12 coin flips of a fair coin, that you get 7 heads?
+'''
+
+'''
+"You have 14 components in a circuit. At any given time, there is a 95% chance that a given component is functioning. What is the probability that 12 components are functioning? Assume that each component functions independently of every other."
+'''
