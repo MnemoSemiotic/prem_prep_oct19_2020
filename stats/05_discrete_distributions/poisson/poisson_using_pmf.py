38d68338a82f3cf1ee16c9433930c664f71ff747
Tovio Roberts <clownfragment@gmail.com>
2020-11-12 19:29:04 -0700

    Auto Updated: Thu 29:19 Nov 12 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -16,4 +16,28 @@ def factorial(n):
 def poisson_pmf(lmbda, k):
     return lmbda**k  *  e**(-lmbda) / factorial(k)
 
-print(poisson_pmf(10, 10))
+# print(poisson_pmf(10, 10))
+
+
+'''
+Examples of Poisson-like phenomena
+
+Phenomenon:
+Given an area of grass, it is likely that the distribution of pill bugs follows a poisson process.
+
+Question:
+PMF: In a square foot of your front yard, on average there are 20 
+roly polys in residence. What is the probability that in a given
+square foot of your front yard, you find 15 roly polys?
+
+
+Phenomenon:
+Cars passing by an intersection at a certain time of day/year, for the duration of a fixed amount of time, will likely follow a Poisson distribution.
+
+Question:
+PMF: A given intersection will have on avg 15 cars pass through in 10 minutes.
+What is the probability that 20 cars pass through in 15 minutes?
+
+CDF: Given the same intersection, what is the probability that more than 15
+cars will pass through in 15 minutes?
+'''
