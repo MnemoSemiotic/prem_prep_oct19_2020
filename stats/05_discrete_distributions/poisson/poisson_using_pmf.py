c09d5e515e4d41939923c4395ca3f0e1cc560814
Tovio Roberts <clownfragment@gmail.com>
2020-11-12 20:46:41 -0700

    Auto Updated: Thu 46:20 Nov 12 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -262,3 +262,58 @@ it will be +2 higher
 
 Add to Z the results of the 8-sided and the 6-sided dice.
 '''
+
+def digit_sum(num):
+    out = 0
+    for dig in str(num):
+        out += int(dig)
+    return out
+
+print(digit_sum(64))
+
+# def outcomes_of_Z():
+#     outcomes = []
+
+#     for i in range(1, 12+1):
+#         for j in range(1, 64+1):
+#             for k in range(1, 8+1):
+#                 for l in range(1, 6+1):
+#                     outcomes.append([i, j, k, l]))
+    
+#     for outcome in outcomes:
+#         Z = 0
+
+#         if outcome[0] % 3 == 0:
+#             Z += 3
+#         if outcome[1]
+
+#     return outcomes
+
+# # print(outcomes_of_A())
+
+# def a_counts_dict():
+#     d = dict()
+
+#     outcomes = outcomes_of_A()
+
+#     for outcome in outcomes:
+#         if outcome not in d:
+#             d[outcome] = 0
+#         d[outcome] += 1
+    
+#     return d
+
+# # for k, v in a_counts_dict().items():
+# #     print(f'{k}: {v}')
+
+# def a_proba_dict():
+#     d = a_counts_dict()
+#     d_out = dict()
+
+#     for k, v in d.items():
+#         d_out[k] = v / sum(d.values())
+
+#     return d_out
+
+# # for k, v in a_proba_dict().items():
+# #     print(f'{k}: {v}')
