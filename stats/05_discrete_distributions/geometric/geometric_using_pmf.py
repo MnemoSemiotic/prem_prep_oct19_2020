439d6b50f23f778cde874f5ab38ac3aa5d8cec20
Tovio Roberts <clownfragment@gmail.com>
2020-11-14 13:05:58 -0700

    Auto Updated: Sat 05:13 Nov 14 2020

--- stats/05_discrete_distributions/geometric/geometric_using_pmf.py
+++ stats/05_discrete_distributions/geometric/geometric_using_pmf.py
@@ -111,5 +111,11 @@ def geometric_pmf_dict(p, k_high, inclusive=True):
     return d
 
 
-for k, v in geometric_pmf_dict(p=0.5, k_high=10, inclusive=True).items():
-    print(f'{k}: {v}')
+# for k, v in geometric_pmf_dict(p=0.5, k_high=10, inclusive=True).items():
+#     print(f'{k}: {v}')
+
+
+'''
+Geometric CDF dict
+'''
+def geometric_cdf_dict(p, k_high, inclusive=True):
