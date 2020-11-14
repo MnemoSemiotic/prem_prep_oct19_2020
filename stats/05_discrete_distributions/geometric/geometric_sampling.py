ed78a57294e2efd510e3d8a3018db008d738b1e8
Tovio Roberts <clownfragment@gmail.com>
2020-11-14 13:21:38 -0700

    Auto Updated: Sat 21:13 Nov 14 2020

--- stats/05_discrete_distributions/geometric/geometric_sampling.py
+++ stats/05_discrete_distributions/geometric/geometric_sampling.py
@@ -21,5 +21,22 @@ def perform_geometric(p=0.5):
     print(f'Success on the {num_trials} trial!')
 
 p=0.00005
-perform_geometric(p)
-print(f'Expectation is {(1)/p} for a probability of {p}')
+# perform_geometric(p)
+# print(f'Expectation is {(1)/p} for a probability of {p}')
+
+
+def geometric(p=0.5):
+    # num of failures prior to success
+    lst = []
+
+    for _ in range(1000000000):
+        flip = bernoulli(p)
+        lst.append(flip)
+
+        if flip == 1:
+            break 
+    
+    return len(lst) - 1
+
+
+print(geometric(p=0.0005))
