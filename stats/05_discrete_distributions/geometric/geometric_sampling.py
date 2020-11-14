580596d3b35fd9552ab2791f350bcb1dd37f819f
Tovio Roberts <clownfragment@gmail.com>
2020-11-14 13:53:28 -0700

    Auto Updated: Sat 53:13 Nov 14 2020

--- stats/05_discrete_distributions/geometric/geometric_sampling.py
+++ stats/05_discrete_distributions/geometric/geometric_sampling.py
@@ -69,7 +69,7 @@ def geometric_samples_proba_dict(p=0.5, num_samples=10000):
 
     return d_out
 
-d = geometric_samples_proba_dict(p=0.05, num_samples=10000)
+d = geometric_samples_proba_dict(p=0.5, num_samples=10000)
 
 for k, v in sorted(d.items()):
     print(f'{k}: {v}')   
