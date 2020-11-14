2d479ece9f04b953f6c7f55ccecd7ba2dc9b09dc
Tovio Roberts <clownfragment@gmail.com>
2020-11-14 13:55:33 -0700

    Auto Updated: Sat 55:13 Nov 14 2020

--- stats/05_discrete_distributions/geometric/geometric_sampling.py
+++ stats/05_discrete_distributions/geometric/geometric_sampling.py
@@ -59,6 +59,7 @@ d = geometric_samples_dict(p=0.05, num_samples=10000)
 # for k, v in sorted(d.items()):
 #     print(f'{k}: {v}')
 
+''' could add a function in here to perform ^ say 500 trials '''
 
 def geometric_samples_proba_dict(p=0.5, num_samples=10000):
     d = geometric_samples_dict(p, num_samples)
