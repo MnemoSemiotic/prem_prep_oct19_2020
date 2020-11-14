699db6277c00aebf40bf2faf62adec2ec5fdb9e9
Tovio Roberts <clownfragment@gmail.com>
2020-11-14 12:35:10 -0700

    Auto Updated: Sat 35:12 Nov 14 2020

--- stats/05_discrete_distributions/geometric/geometric_using_pmf.py
+++ stats/05_discrete_distributions/geometric/geometric_using_pmf.py
@@ -53,5 +53,12 @@ A roofer hits their thumb with a hammer 1/1000 times when they swing the hammer.
 What is the probability that the roofer will first hit their thumb 
 after swinging the hammer 37 times (hits on the 38th)?
 '''
-print(geometric_pmf(1/1000, 38, inclusive=True))
-print(geometric_pmf(1/1000, 37, inclusive=False))
+# print(geometric_pmf(1/1000, 38, inclusive=True))
+# print(geometric_pmf(1/1000, 37, inclusive=False))
+
+
+
+'''
+Geometric cdf
+'''
+def geom_cdf_accum(p, k, inclusive=True):
