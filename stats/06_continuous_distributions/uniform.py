8fe1b85186d65e703e273104f4eefda19b349e10
Tovio Roberts <clownfragment@gmail.com>
2020-11-19 19:29:23 -0700

    Auto Updated: Thu 29:19 Nov 19 2020

---
+++ stats/06_continuous_distributions/uniform.py
@@ -0,0 +1,27 @@
+from random import choice
+
+def get_bit():
+    return choice([0,1])
+
+
+def get_binary(n=8):
+    return_list = []
+
+    for _ in range(n):
+        return_list.append(get_bit())
+
+    return return_list
+
+
+def get_float(n=64):
+    bin_list = get_binary(n)
+
+    float_accum = 0.0
+
+    for idx, bit in enumerate(bin_list, 1):
+        float_accum += bit * 0.5**idx
+
+    return float_accum
+
+
+print(get_float())
