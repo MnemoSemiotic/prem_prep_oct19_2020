1109d919033dd6c6c8a26f51082ecf53fc24ae3a
Tovio Roberts <clownfragment@gmail.com>
2020-11-19 19:55:15 -0700

    Auto Updated: Thu 55:19 Nov 19 2020

---
+++ stats/06_continuous_distributions/exponential.py
@@ -0,0 +1,20 @@
+from math import e, sqrt
+
+def exponential_pdf(lmbda, x):
+    pass
+
+
+def exponential_cdf(lmbda, x):
+    pass
+
+
+def exponential_mean(lmbda):
+    pass
+
+
+def exponential_variance(lmbda):
+    pass
+
+
+def exponential_std(lmbda):
+    pass
